357e11aa560cf843ae39512e5aaf7af5ec16e43e
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-09 19:04:11 +0200

    Snack 2 completato

--- Snack2/script.js
+++ Snack2/script.js
@@ -7,3 +7,48 @@
     Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti 
     e stampiamo tutto in console.
 */
+
+/*****************
+    DEFINIZIONI
+******************/
+
+// Array di oggetti
+const squadre = [
+    { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 }
+];
+
+// Nuovo array che conterrà solo nome + falliSubiti
+const nuovoArraySquadre = [];
+
+
+/*****************
+    ELABORAZIONE
+******************/
+
+for (let squadra of squadre) {
+    squadra.puntiFatti = genNumRandom(0, 10);
+    squadra.falliSubiti = genNumRandom(0, 20);
+
+    // Aggiungo al nuovo array
+    nuovoArraySquadre.push ( { nome: squadra.nome, falliSubiti: squadra.falliSubiti} );
+}
+
+console.log("ARRAY SQUADRE");
+console.log(squadre);
+console.log("NUOVO ARRAY SQUADRE");
+console.log(nuovoArraySquadre);
+
+
+
+/*****************
+    FUNZIONI
+******************/
+
+// Generazione numero random con min e max inclusi
+function genNumRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+} 
